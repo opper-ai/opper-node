a5308740d9a1933c169571932e185f8e95ac93e9
christopher-weir <christopher_weir@me.com>
2024-08-09 07:54:03 +0200

    Better support for cjs and esm

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
     entry: ["src/index.ts", "!src/**/*.test.ts"],
-    splitting: false,
+    format: ["cjs", "esm"], // Output both CommonJS and ES Modules
+    dts: true, // Generate declaration files
+    splitting: false, // Disable code splitting for ESM
     sourcemap: true,
     clean: true,
-    // Generate declaration file
-    dts: true,
+    target: ["node18", "es2020"], // Target Node.js 14+ and modern browsers
+    outDir: "dist",
 });
