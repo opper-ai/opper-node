9d0ea1db8c5a9b19ec38edaff51aa8e3caeac08c
christopher-weir <christopher_weir@me.com>
2024-09-16 13:50:34 +0200

    Index as a class

--- src/indexes.ts
+++ src/indexes.ts
@@ -1,6 +1,83 @@
+import type Client from "./index";
 import { OpperIndexDocument, OpperIndexFilter, OpperIndex } from "./types";
 
 import APIResource from "./api-resource";
+
+export class Index extends APIResource {
+    public _index: OpperIndex;
+    protected _client: Client;
+
+    constructor(index: OpperIndex, client: Client) {
+        super(client);
+        this._client = client;
+        this._index = index;
+    }
+
+    /**
+     * Adds a document to an index.
+     * @param index The index to add the document to.
+     * @param document The document to add to the index.
+     * @throws {APIError} If the response status is not 200.
+     *
+     * @example
+     * ```ts
+     * const index = await client.indexes.create('support-tickets');
+     * const document = { title: 'My first support ticket', content: 'This is my first support ticket.' };
+     * await index.add(document);
+     * ```
+     */
+    public async add(document: OpperIndexDocument): Promise<void> {
+        await this.doPost(this.calcURLAddIndex(this._index.uuid), document);
+    }
+
+    /**
+     * Queries the most relevant documents from an index based on semantic similarity to a query.
+     * @param query The query to retrieve documents for.
+     * @param k The maximum number of documents to retrieve.
+     * @param filters The filters to apply to the documents in the index.
+     * @returns The most relevant documents from the index
+     * @throws {APIError} If the response status is not 200.
+     *
+     * @example
+     * ```ts
+     * const documents = await index.query({ query: 'I cannot log in to my account', k: 10 });
+     * ```
+     */
+    public async query({
+        query,
+        k,
+        filters,
+        parent_span_uuid,
+    }: {
+        /**
+         * The query to retrieve documents for.
+         */
+        query: string;
+        /**
+         * The maximum number of documents to retrieve.
+         */
+        k: number;
+        /**
+         * The filters to apply to the documents in the index.
+         */
+        filters?: OpperIndexFilter[];
+        /**
+         * The parent span uuid to use for the request.
+         */
+        parent_span_uuid?: string;
+    }): Promise<OpperIndexDocument[]> {
+        const response = await this.doPost(this.calcURLQueryIndex(this._index.uuid), {
+            q: query,
+            k: k,
+            filters: filters,
+            parent_span_uuid,
+        });
+
+        const documents: OpperIndexDocument[] = await response.json();
+
+        return documents;
+    }
+}
 
 class Indexes extends APIResource {
     /**
@@ -20,30 +97,13 @@ class Indexes extends APIResource {
     }
 
     /**
-     * Retrieves an index by name from the OpperAI API.
-     * @param name The name of the index to retrieve.
-     * @returns A promise that resolves to the index with the given name, or null if no index with the given name exists.
-     * @throws {APIError} If the response status is not 200.
-     */
-    public async get(name: string): Promise<OpperIndex | null> {
-        const list = await this.list();
-
-        const index = list.find((index) => index.name === name);
-
-        if (!index) {
-            return null;
-        }
-        return index;
-    }
-
-    /**
      * Creates an index in the OpperAI API.
      * @param name The name of the index to create.
      * @param embedding_model Optional. The embedding model to use for the index.
      * @returns A promise that resolves to the created index.
      * @throws {APIError} If the response status is not 200.
      */
-    public async create(name: string, embedding_model?: string): Promise<OpperIndex> {
+    public async create(name: string, embedding_model?: string): Promise<Index> {
         const response = await this.doPost(this.calcURLIndexes(), {
             name,
             ...(embedding_model && { embedding_model }),
@@ -51,7 +111,7 @@ class Indexes extends APIResource {
 
         const data = await response.json();
 
-        return data;
+        return new Index(data, this._client);
     }
 
     /**
@@ -65,48 +125,19 @@ class Indexes extends APIResource {
     }
 
     /**
-     * Adds a document to an index.
-     * @param index The index to add the document to.
-     * @param document The document to add to the index.
-     * @throws {APIError} If the response status is not 200.
-     *
-     * @example
-     * ```ts
-     * const index = await client.indexes.create('support-tickets');
-     * const document = { title: 'My first support ticket', content: 'This is my first support ticket.' };
-     * await client.indexes.add(index, document);
-     * ```
-     */
-    public async add(index: OpperIndex, document: OpperIndexDocument): Promise<void> {
-        await this.doPost(this.calcURLAddIndex(index.uuid), document);
-    }
-
-    /**
-     * Retrieves the most relevant documents from an index based on semantic similarity to a query.
-     * @param index The index to retrieve documents from.
-     * @param query The query to retrieve documents for.
-     * @param k The maximum number of documents to retrieve.
-     * @param filters The filters to apply to the documents in the index.
-     * @returns The most relevant documents from the index
+     * Retrieves an index by name.
+     * @param name The name of the index to retrieve.
+     * @returns A promise that resolves to the index.
      * @throws {APIError} If the response status is not 200.
      */
-    public async retrieve(
-        index: OpperIndex,
-        query: string,
-        k: number,
-        filters: OpperIndexFilter[] | null,
-        parent_span_uuid?: string | null
-    ): Promise<OpperIndexDocument[]> {
-        const response = await this.doPost(this.calcURLQueryIndex(index.uuid), {
-            q: query,
-            k: k,
-            filters: filters,
-            parent_span_uuid,
-        });
+    public async get(name: string) {
+        const list = await this.list();
+        const index = list.find((index) => index.name === name);
 
-        const documents: OpperIndexDocument[] = await response.json();
-
-        return documents;
+        if (!index) {
+            return null;
+        }
+        return new Index(index, this._client);
     }
 }
 
