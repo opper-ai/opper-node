381c4eed4805e02db99cbaaf65d193da53aab46c
Johnny Chadda <johnny@chadda.se>
2024-09-30 16:45:14 +0200

    Add parameters to image generation for choosing model and parameters

--- src/types.ts
+++ src/types.ts
@@ -64,15 +64,15 @@ export type OpperIndexFile = {
     original_filename: string;
     size: number;
     index_status:
-        | "init"
-        | "pending"
-        | "uploading"
-        | "indexing"
-        | "success"
-        | "indexed"
-        | "error"
-        | "failed"
-        | "invalid";
+    | "init"
+    | "pending"
+    | "uploading"
+    | "indexing"
+    | "success"
+    | "indexed"
+    | "error"
+    | "failed"
+    | "invalid";
     n_vectors: number;
 };
 
@@ -123,12 +123,11 @@ export type OpperExample = {
     comment?: string;
 };
 
-export type OpperGenerateImage = {
+export interface OpperGenerateImage {
     prompt: string;
-    image_size?: string;
-    quality?: string;
-    parent_span_uuid?: string;
-};
+    model?: string;
+    parameters?: Record<string, unknown>;
+}
 
 export type OpperCall = {
     /**
